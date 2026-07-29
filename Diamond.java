c6f5523e7bda77c4c23863458f43a4f8f9ce0bd6
Devika <devikasasidharan68@gmail.com>
2026-07-29 11:55:31 +0530

    Initial DSA programs

---
+++ Diamond.java
@@ -0,0 +1,33 @@
+package patterns;
+import java.util.*;
+
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j =n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for(int k =1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k =n-1; k >= i; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        
+        }
+        
+
+        
+    }
+    
+}
---
+++ pattern1.java
@@ -0,0 +1,22 @@
+package patterns;
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+
+            }
+            for(int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
